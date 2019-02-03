82aba4b5085969c62912000763d3643e2047bf42
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-02-03 01:13:08 +0000

    Reuse useState() for decrement of counter

--- src/Counter.js
+++ src/Counter.js
@@ -3,5 +3,12 @@ import React, { useState } from 'react';
 export default function Counter() {
   const [count, setCount] = useState(0);
   const increment = () => setCount(count + 1);
-  return <button onClick={increment}>{count}</button>;
+  const decrement = () => setCount(count - 1)
+  return (
+    <span className="controls">
+      <button onClick={increment}>Up</button>
+        {count}
+      <button onClick={decrement}>Down</button>
+    </span>
+  )
 }
