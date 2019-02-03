6061020facd542b3eb07368b6aade1dd9f927756
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-02-03 21:17:09 +0000

    Refactor useState into custom hook

--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 
+function useCounter({initialState, step}) {
+  const [count, setCount] = useState(initialState);
+  const increment = () => setCount(count + step);
+  const decrement = () => setCount(count - step);
+  return {count, increment, decrement}
+}
+
 export default function Counter() {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1)
+  const {count, increment, decrement} = useCounter({initialState: 0, step: 1})
   return (
     <span className="controls">
       <button onClick={increment}>Up</button>
