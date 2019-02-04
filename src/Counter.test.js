afb66cd6b5113684eb08d080ea18395cbf4bb875
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-02-04 00:03:02 +0000

    Test useEffect with local storage
    * Issue with cleanup between tests

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { render, fireEvent, cleanup } from 'react-testing-library';
 import Counter from './Counter';
 
-afterEach(cleanup)
+afterEach(cleanup);
+afterEach(() => {
+  window.localStorage.removeItem('count')
+});
 
 test('Counter increments the count', () => {
   const { container, getByText, getByTestId } = render(<Counter />);
@@ -18,5 +21,17 @@ test('Counter decrements the count', () => {
   const count = getByTestId('count');
   fireEvent.click(button);
   fireEvent.click(button);
-  expect(count.textContent).toBe('-2');
+  expect(count.textContent).toBe('-1');
+});
+
+test('reads and updates localStorage', () => {
+  window.localStorage.setItem('count', 10);
+  const { container, getByText, getByTestId, rerender } = render(<Counter />);
+  const button = getByText('Up');
+  const count = getByTestId('count');
+  expect(count.textContent).toBe('10');
+  fireEvent.click(button);
+  expect(count.textContent).toBe('11');
+  rerender(<Counter />);
+  expect(window.localStorage.getItem('count')).toBe('11');
 });
