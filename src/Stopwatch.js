e5631cfc4aa299dff1b05b103dc0e6a59e21b52f
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-02-04 23:12:46 +0000

    Add useReducer with setState
    * Replace multiple useState

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,22 +1,19 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useReducer, useRef, useEffect} from 'react';
 
-const buttonStyles = {
-  border: '1px solid #ccc',
-  background: '#fff',
-  fontSize: '2em',
-  padding: 15,
-  margin: 5,
-  width: 200,
+function reducer(currentState, newState) {
+  return {...currentState, ...newState}
 }
 
 export default function Stopwatch() {
-  const [lapse, setLapse] = useState(0);
-  const [running, setRunning] = useState(false);
-  const intervalRef = useRef(null)
+  const [{running, lapse}, setState] = useReducer(reducer, {
+    running: false,
+    lapse: 0,
+  })
+  const intervalRef = useRef(null);
 
   useEffect(() => {
     return () => clearInterval(intervalRef.current)
-  }, [])
+  }, []);
 
   function handleRunClick() {
     if (running) {
@@ -24,16 +21,15 @@ export default function Stopwatch() {
     } else {
       const startTime = Date.now() - lapse;
       intervalRef.current = setInterval(() => {
-        setLapse(Date.now() - startTime)
+        setState({lapse: Date.now() - startTime})
       }, 0)
     }
-    setRunning(!running)
+    setState({running: !running});
   }
 
   function handleClearClick() {
     clearInterval(intervalRef.current);
-    setLapse(0);
-    setRunning(false);
+    setState({lapse: 0, running: false});
   }
 
   return (
@@ -51,4 +47,14 @@ export default function Stopwatch() {
       <button onClick={handleClearClick} style={buttonStyles}>Clear</button>
     </div>
   )
-}
+};
+
+
+const buttonStyles = {
+  border: '1px solid #ccc',
+  background: '#fff',
+  fontSize: '2em',
+  padding: 15,
+  margin: 5,
+  width: 200,
+};
